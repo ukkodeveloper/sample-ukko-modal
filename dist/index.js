c407941d6f849d905e4d8789fd52e7c562c8fbf4
ukkodeveloper <ukkodeveloper@gmail.com>
2023-05-02 13:18:25 +0900

    trial

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "MyModal", {
+  enumerable: true,
+  get: function get() {
+    return _MyModal.default;
+  }
+});
+var _MyModal = _interopRequireDefault(require("./MyModal"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
